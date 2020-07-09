1a44bfd01842ccf3751797216fa8f081b52620f9
mrhossan1988 <mrhossan1988@gmail.com>
2020-07-09 13:52:24 +0200

    slice,splice,join a array

--- arguments.js
+++ arguments.js
@@ -1,20 +1,9 @@
- function stopWatch(){
-    let count = 0;
-    return function(){
-       count++;
-       return count;
-    }
- }
- const clock1 = stopWatch();
- console.log(clock1());
- console.log(clock1());
- console.log(clock1());
- console.log(clock1());
- console.log(clock1());
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const takeOfSide = num.slice(2, 5);
+const removeAndPut = num.splice(2, 2, 50, 60, 70)
+const together = num.join(" hello ")
 
- const clock2 = stopWatch();
- console.log(clock1());
- console.log(clock2());
- console.log(clock2());
- console.log(clock1());
- console.log(clock1());
+console.log(takeOfSide)
+console.log(num)
+console.log(removeAndPut)
+console.log(together)
