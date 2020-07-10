074d34224fd6091e1738346ffe2ca6585178140c
mrhossan1988 <mrhossan1988@gmail.com>
2020-07-10 17:46:08 +0200

    object bind

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
- 
+ const person = {
+     fName: "hero",
+     lName: "man",
+     salary: 15000,
+      getFullName: function(){
+          console.log(this.fName, this.lName);
+      },
+      chargeBill: function(amount){
+          console.log(this)
+       this.salary = this.salary - amount;
+       return this.salary;
+      }
+ }
+const heroPerson = {
+    firstName: "hero",
+    lastName: "fighter",
+    salary: 50000
+}
+const friendlyPerson = {
+    firstName: "hero",
+    lastName: "king",
+    salary: 70000
+}
+person.chargeBill();
+const heroChargeBil = person.chargeBill.bind(heroPerson);
+heroChargeBil(2000);
+
+console.log(heroPerson.salary)
+
+const friendlyChargeBill = person.chargeBill.bind(friendlyPerson);
+friendlyChargeBill(2000);
+console.log(friendlyPerson.salary)
