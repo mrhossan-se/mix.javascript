a77f8b6ddf219b76feb53632c55e37a840f51ba8
mrhossan1988 <mrhossan1988@gmail.com>
2020-07-10 19:51:48 +0200

    clacc object with new key word

--- index.js
+++ index.js
@@ -1,40 +1,21 @@
- const person = {
-     fName: "hero",
-     lName: "man",
-     salary: 15000,
-      getFullName: function(){
-          console.log(this.fName, this.lName);
-      },
-      chargeBill: function(amount, tex, tips){
-         //console.log(this)
-       this.salary = this.salary - amount - tex - tips;
-       return this.salary;
-      }
- }
-const heroPerson = {
-    firstName: "hero",
-    lastName: "fighter",
-    salary: 50000
+class Person {
+    constructor(fName, lName, salary){
+        this.fName = fName;
+        this.lName = lName;
+        this.salary = salary;
+    }
 }
-const friendlyPerson = {
-    firstName: "hero",
-    lastName: "king",
-    salary: 70000
+
+const heroPerson = new Person("Hero", "Balam", 50000);
+console.log(heroPerson);
+
+class Woman {
+    constructor(firstName, lastName, salary){
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.salary = salary;
+    }
 }
-// person.chargeBill();
-// const heroChargeBil = person.chargeBill.bind(heroPerson);
-// heroChargeBil(2000);
 
-// console.log(heroPerson.salary)
-
- const friendlyChargeBill = person.chargeBill.bind(friendlyPerson);
-friendlyChargeBill(2000);
- console.log(friendlyPerson.salary)
- person.chargeBill.call(heroPerson, 900, 100, 10)
- person.chargeBill.call(heroPerson, 3000)
-person.chargeBill.call(friendlyPerson, 1000, 200, 50)
-person.chargeBill.apply(friendlyPerson, [1000, 200, 50])
-person.chargeBill.apply(heroPerson, [100, 200, 300])
-console.log(heroPerson.salary)
-console.log(friendlyPerson.salary)
-console.log(heroPerson.salary)
+const leady = new Woman("Miss", "Beauty", 50000);
+console.log(leady);
