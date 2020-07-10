aa2fdfe879d3b48527369b51ff951012315664b8
mrhossan1988 <mrhossan1988@gmail.com>
2020-07-10 18:58:21 +0200

    bind, call, apply

--- index.js
+++ index.js
@@ -5,9 +5,9 @@
       getFullName: function(){
           console.log(this.fName, this.lName);
       },
-      chargeBill: function(amount){
-          console.log(this)
-       this.salary = this.salary - amount;
+      chargeBill: function(amount, tex, tips){
+         //console.log(this)
+       this.salary = this.salary - amount - tex - tips;
        return this.salary;
       }
  }
@@ -21,12 +21,20 @@ const friendlyPerson = {
     lastName: "king",
     salary: 70000
 }
-person.chargeBill();
-const heroChargeBil = person.chargeBill.bind(heroPerson);
-heroChargeBil(2000);
+// person.chargeBill();
+// const heroChargeBil = person.chargeBill.bind(heroPerson);
+// heroChargeBil(2000);
 
-console.log(heroPerson.salary)
+// console.log(heroPerson.salary)
 
-const friendlyChargeBill = person.chargeBill.bind(friendlyPerson);
+ const friendlyChargeBill = person.chargeBill.bind(friendlyPerson);
 friendlyChargeBill(2000);
+ console.log(friendlyPerson.salary)
+ person.chargeBill.call(heroPerson, 900, 100, 10)
+ person.chargeBill.call(heroPerson, 3000)
+person.chargeBill.call(friendlyPerson, 1000, 200, 50)
+person.chargeBill.apply(friendlyPerson, [1000, 200, 50])
+person.chargeBill.apply(heroPerson, [100, 200, 300])
+console.log(heroPerson.salary)
 console.log(friendlyPerson.salary)
+console.log(heroPerson.salary)
