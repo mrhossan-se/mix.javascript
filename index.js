6037b12f170aa10fd17a5928a0087e52776d5fb7
mrhossan1988 <mrhossan1988@gmail.com>
2020-07-14 19:25:51 +0200

    string reverse

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
- 
-//  var speech = "i am a good person. i like to good person";
-//  var count = 0;
-//  for (let i = 0; i < speech.length; i++) {
-//    const char = speech[i];
-//    if (char == " ") {
-//      count++;
-//    }
-//  }
-//  console.log(count)//answer is =  9
-  
- var speech = "i am     a good    person i like to good person";
- var count = 0;
- for (let i = 0; i < speech.length; i++) {
-   const char = speech[i];
-   if (char == " " && speech[i-1] != " ") {
-     count++;
-   }
- }
-//count++;
- console.log(count)
+
+function reversString(str) {
+  var reverse = "";
+for (let i = 0; i < str.length; i++) {
+  const char = str[i];
+  reverse = char + reverse;
+}
+ return reverse;
+}
+
+var statement = "Hello Alien brother how are you";
+var  Alien = reversString(statement)
+console.log(Alien)
