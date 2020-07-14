cb27ac5133c5c3c138976a052882f9fb875fe8b1
mrhossan1988 <mrhossan1988@gmail.com>
2020-07-14 18:14:03 +0200

    sum of total numbers

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-var marks = [19, 29, 39, 49, 59, 69, 79, 89, 99, 00];
-var max = marks[0];
-for (let i = 0; i < marks.length; i++) {
-  var element = marks[i];
-  if (element > max) {
-    max = element;
-  }
+var numbers = [19, 29, 39, 49, 59, 91, 96, 89, 99, 00];
+
+var sum = 0;
+for(var i = 0; i < numbers.length; i++){
+  var element = numbers[i];
+  sum = sum + element;
 }
-console.log("hightest value is ", max)
+ console.log("total of the number : ", sum)
+
+
+ function getArrySum (numbers){
+   var sum = 0;
+   for(var i = 0; i <numbers.length; i++ ){
+     var element = number[i];
+     sum = sum+element;
+   }
+   return sum;
+ }
+ var number = [18, 29, 79, 49, 79, 91, 96, 79, 99, 10];
+ var result = getArrySum(number)
+ console.log(" total of the number is : ",result)
+ 
+ var results = getArrySum([33, 44, 55, 66, 77]);
+ console.log(results)
