0d658f93012056e554a759a5fe96ffcac7787800
mrhossan1988 <mrhossan1988@gmail.com>
2020-07-14 18:34:03 +0200

    remove duplicate number

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
-var numbers = [19, 29, 39, 49, 59, 91, 96, 89, 99, 00];
-
-var sum = 0;
-for(var i = 0; i < numbers.length; i++){
-  var element = numbers[i];
-  sum = sum + element;
+var numbers = [19, 19, 29, 39, 49, 59, 91, 96, 89, 99, 19, 39, 00];
+var uniqueName = [];
+for (let i = 0; i < numbers.length; i++) {
+  const element = numbers[i];
+  var index = uniqueName.indexOf(element);
+  if (index == -1) {
+    uniqueName.push(element)
+  }
+  
 }
- console.log("total of the number : ", sum)
-
-
- function getArrySum (numbers){
-   var sum = 0;
-   for(var i = 0; i <numbers.length; i++ ){
-     var element = number[i];
-     sum = sum+element;
-   }
-   return sum;
- }
- var number = [18, 29, 79, 49, 79, 91, 96, 79, 99, 10];
- var result = getArrySum(number)
- console.log(" total of the number is : ",result)
- 
- var results = getArrySum([33, 44, 55, 66, 77]);
- console.log(results)
+console.log(uniqueName) 
