e973b4f7037a760435acbea5b1d1c303821ec248
mrhossan1988 <mrhossan1988@gmail.com>
2020-07-14 17:46:19 +0200

    math and max number

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-var num = 2 ;
-var number = 2.5;
+var a = 600;
+var b = 700;
+var c = 500;
 
-var result = Math.floor(number);
-var result1 = Math.ceil(number);
-var result2 = Math.round(number);
-var result3 = Math.random(number);
-var randomNumber = Math.random(number) * 6;
-var randomRoundNumber = Math.round(randomNumber);
-
-for (let i = 0; i < 10; i++) {
-  var randomNum = Math.random(number) * 10;
-var randomRoundNumber = Math.round(randomNum);
-console.log(randomRoundNumber);
+if (a > b) {
+  console.log("a is bigger  ")
 }
+else if(b > c){
+  console.log("b is bigger ")
+}
+else{
+  console.log("c is bigger  ")
+}
+
+var max = Math.max(a, b, c)
+console.log(max)
